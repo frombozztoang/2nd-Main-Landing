70f173d3a025f503d6fec4232e317602a1f55828
frombozztoang <frombozztoang@gmail.com>
2024-01-16 03:03:14 +0000

    Chore: chore

---
+++ src/app/services/loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Basketball from "../../../public/images/basketball.png";
+import Image from "next/image";
+
+const loading = () => {
+  return (
+    <div className="transition-all flex flex-col items-center justify-center min-h-screen font-bold">
+      <Image
+        src={Basketball}
+        alt="loading basketball"
+        className="tablet:w-390 tablet:h-390 w-162 h-162 "
+      />
+      loading...
+    </div>
+  );
+};
+
+export default loading;
